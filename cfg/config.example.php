9e237a22b131dd074c163d8d55c74e1a97a489b0
Owen Dyckhoff <owen.dyckhoff@gmail.com>
2019-05-04 10:39:01 +0100

    renamed example config file, added more options

--- cfg/config.example.php
+++ cfg/config.example.php
@@ -1,5 +1,10 @@
 <?php
 
+	define('DEV_ENV', FALSE);
+
+	define('BASE_URI', '');
+	define('PROTOCOL', 'http://');
+
 	define('DBUSER', "username");
 	define('DBPASS', "password");
 	define('DBHOST', "host.example.com");
